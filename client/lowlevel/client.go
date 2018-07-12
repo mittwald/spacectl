91300176e1d2578f817974b7c7aeef86b83bcb02
Henning Surmeier <h.surmeier@mittwald.de>
2018-07-12 19:55:46 +0200

    omit 403 check for get as well

--- client/lowlevel/client.go
+++ client/lowlevel/client.go
@@ -11,8 +11,6 @@ import (
 	"regexp"
 	"strings"
 	"time"
-
-	"github.com/mittwald/spacectl/service/auth"
 )
 
 var versionRegexp = regexp.MustCompile("^/v[0-9]+/")
@@ -66,10 +64,6 @@ func (c *SpacesLowlevelClient) Get(path string, target interface{}) error {
 	}
 
 	defer res.Body.Close()
-
-	if res.StatusCode == 403 {
-		return auth.InvalidCredentialsErr{}
-	}
 
 	if res.StatusCode >= 400 {
 		msg := Message{}
